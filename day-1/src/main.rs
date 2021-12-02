abd6cc93da422784e4eaecd0a678aeee93bed365
Epsylon <eepsylon.3@gmail.com>
2021-12-02 18:53:23 +0200

    use aoclib in day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,12 +1,7 @@
-use std::io::{stdin, BufRead, BufReader};
-
-fn main() {
-    let reader = BufReader::new(stdin());
-
+fn task1(values: Vec<u16>) {
     let mut increased_count = 0;
     let mut prev_height = None;
-    for line in reader.lines() {
-        let height: u16 = line.unwrap().parse().unwrap();
+    for height in values {
         match prev_height {
             Some(prev_height) if prev_height < height => increased_count += 1,
             _ => {}
@@ -16,4 +11,25 @@ fn main() {
     }
 
     println!("{increased_count}");
+}
+
+fn task2(values: Vec<u16>) {
+    let increased_count: usize = values
+        .windows(4)
+        .map(|win| {
+            let a: u16 = win[0..3].iter().sum();
+            let b: u16 = win[1..4].iter().sum();
+
+            if b > a { 1 } else { 0 }
+        })
+        .sum();
+
+    println!("{increased_count}");
+}
+
+fn main() {
+    aoclib::AocTask::read_lines(|line| line.parse::<u16>().unwrap())
+        .task1(task1)
+        .task2(task2)
+        .run();
 }
